7a8dcf357a705f9751a47145700f534b3bba743a
Andreas Kupries <andreas.kupries@suse.com>
2017-05-05 12:03:41 -0700

    Fix various typos.

--- set-check.sh
+++ set-check.sh
@@ -3,6 +3,6 @@
 target="$1"
 secdir="${2:-../cloudfoundry/secure}"
 
-fly -t "$target" set-pipeline -p stampy-check -c stampy-check.yml \
+fly -t "$target" set-pipeline -p stampy-check -c stampy-check.yaml \
     -l <(gpg -d --no-tty "${secdir}/concourse-secrets.yml.gpg" \
          2> /dev/null)
--- set-master.sh
+++ set-master.sh
@@ -3,6 +3,6 @@
 target="$1"
 secdir="${2:-../cloudfoundry/secure}"
 
-fly -t "$target" set-pipeline -p stampy-master -c stampy-master.yml \
+fly -t "$target" set-pipeline -p stampy-master -c stampy-master.yaml \
     -l <(gpg -d --no-tty "${secdir}/concourse-secrets.yml.gpg" \
          2> /dev/null)
