3981a2c9b60bca1d646b4c5114be10bcc9ae9ff0
Andreas Kupries <andreas.kupries@suse.com>
2017-05-09 10:41:15 -0700

    Added proper checks for the secrets file before trying to use it.

--- set-check.sh
+++ set-check.sh
@@ -2,7 +2,14 @@
 
 target="$1"
 secdir="${2:-../cloudfoundry/secure}"
+secfile="concourse-secrets.yml.gpg"
+secrets="${secdir}/${secfile}"
+
+if [ ! -f "${secrets}" ]
+then
+    echo -e 1>&2 "$0: Failed to find the secrets file ${secrets}\n\tPlease specify the correct directory holding \"${secfile}\"."
+    exit 1
+fi
 
 fly -t "$target" set-pipeline -p stampy-check -c stampy-check.yaml \
-    -l <(gpg -d --no-tty "${secdir}/concourse-secrets.yml.gpg" \
-         2> /dev/null)
+    -l <(gpg -d --no-tty "${secrets}" 2> /dev/null)
