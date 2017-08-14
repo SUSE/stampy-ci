7bb3279b69e8296070708a9aa46c7ce1defd222c
Alex Harford <alex.harford@suse.com>
2017-08-14 13:22:38 -0700

    Handle unset variables better
    * target is now optional
    * Unset CONCOURSE_SECRETS_FILE shows a nicer error message

--- set-check.sh
+++ set-check.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-target="$1"
+set -o nounset
+set -o errexit
+
+# EV 'PIPELINE_PREFIX' = prefix to pipeline name for local customization of test
+#           pipelines
+
+pipeline_name=stampy-check
+
+target="${1:-}"
 
 if test -n "${CONCOURSE_SECRETS_FILE:-}"; then
     if test -r "${CONCOURSE_SECRETS_FILE:-}" ; then
@@ -9,7 +17,15 @@ if test -n "${CONCOURSE_SECRETS_FILE:-}"; then
         printf "ERROR: Secrets file %s is not readable\n" "${CONCOURSE_SECRETS_FILE}" >&2
         exit 2
     fi
+else
+    echo "ERROR: CONCOURSE_SECRETS_FILE location is not set" >&2
+    exit 3
 fi
 
-fly -t "$target" set-pipeline -p stampy-check -c stampy-check.yml \
+fly \
+    ${target:+"--target=${target}"} \
+    set-pipeline \
+    -p "${PIPELINE_PREFIX:-}${pipeline_name}" \
+    -c "${pipeline_name}.yml" \
+    -v s3-bucket=cf-opensusefs2 \
     -l <(gpg -d --no-tty "${secrets_file}" 2> /dev/null)
